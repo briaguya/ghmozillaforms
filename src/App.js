021eb227510bea020b687107efbc13a2b2e18f9a
Brian Smith <briaguya@gmail.com>
2017-08-28 20:48:56 -0500

    links to issue when created

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ import schema from './schema';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {newIssueCreated: false};
+    this.state = {newIssueURL: null};
   }
 
   handleSubmit(e) {
-    this.setState({newIssueCreated: true});
-    console.log(e.formData);
     var headers = new Headers();
-    headers.append('Authorization', 'token v1.086ca559d9650859999960e49ef07aa882f44352');
+    headers.append('Authorization', 'token v1.19486cf990bc967c711d7b181b64a9e6b53af9db');
     headers.append('Content-Type', 'application/vnd.github.v3+json');
 
     fetch('https://api.github.com/repos/briaguya/ghmozillaforms/issues', 
@@ -24,26 +22,20 @@ class App extends Component {
       mode: 'cors',
       headers: headers,
       body: JSON.stringify(e.formData)
-    }).then(function(response) {
-      console.log(response);
-      response.json().then(function(data) {
-        var blob = data;
-        console.log(blob);
+    }).then(response => response.json())
+      .then(json => {
+        this.setState({newIssueURL: json.html_url});
+        console.log(this.state.newIssueURL);
       });
-      console.log(response);
-    }
-
-    );
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.getAuth.bind()}/>
         <Form schema={schema} onSubmit={this.handleSubmit.bind(this)} /> 
-        {this.state.newIssueCreated ?
+        {this.state.newIssueURL ?
           <div>
-            TODO: LINK TO ISSUE
+            <a href={this.state.newIssueURL}>New Issue Created</a>
           </div> : null
         }
       </div>
